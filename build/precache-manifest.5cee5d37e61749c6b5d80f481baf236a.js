7bb9511335c51b8b282eaeb030c6396590a0f976
rydawnshootingstar <rydawnshootingstar@gmail.com>
2020-10-08 14:08:28 -0400

    added build

---
+++ build/precache-manifest.5cee5d37e61749c6b5d80f481baf236a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86407d76f4b6a16b095e12b55058425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f5917b1a2b8b6233e4d",
+    "url": "/static/css/main.812602da.chunk.css"
+  },
+  {
+    "revision": "dec26ef34c34e03026bb",
+    "url": "/static/js/2.9c32d582.chunk.js"
+  },
+  {
+    "revision": "4f5917b1a2b8b6233e4d",
+    "url": "/static/js/main.e0d3bfd4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
